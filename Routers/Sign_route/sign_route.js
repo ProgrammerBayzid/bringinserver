91ac05cf7ba670833b5977112087b94d62b832b2
tanvirmhamud <shakilhassan887@gmail.com>
2023-06-08 09:47:36 +0600

    fast commit

--- Routers/Sign_route/sign_route.js
+++ Routers/Sign_route/sign_route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const axios = require("axios");
 const { User } = require("../..//Model/userModel");
-const { Otp } = require("../../Model/userModel");
+const { Otp } = require("../../Model/otpModel");
 
 const app = express();
 
