e2b2f2ca89acb63bb8a9b9d574235aabb60cae43
tanvirmhamud <shakilhassan887@gmail.com>
2023-06-25 10:01:37 +0600

    fast commit

--- Routers/AdminPanel/adminpa.js
+++ Routers/AdminPanel/adminpa.js
@@ -249,6 +249,84 @@ app.delete("/admin/subject/:id", async (req, res) => {
 
 
 
+app.post('/education_lavel', async (req, res)=> {
+  try {
+   var data = await EducationLavel.findOne(req.body)
+   if (data == null) {
+      await EducationLavel(req.body).save(); 
+      res.status(200).json({message: "add successfull"})
+   }else{
+       res.status(200).json({message: "all ready added"})
+   }
+  } catch (error) {
+   res.status(400).send(error)
+  }
+})
+
+
+app.get('/education_lavel', async (req, res) => {
+   try {
+       var data = await EducationLavel.find().populate([{path: "digree",select: "-subject"}]);
+               res.status(200).send(data)
+
+   } catch (error) {
+       res.send(error);
+   }
+})
+
+
+
+
+
+
+app.post('/digree_add', async (req, res)=> {
+   try {
+    var data = await Digree.findOne({name: req.body.name})
+    if (data == null) {
+       var digreedata = await Digree({name: req.body.name, education: req.body.education});
+       digreedata.save();
+       await EducationLavel.findOneAndUpdate({_id: req.body.education}, {$push: {digree:digreedata._id }})
+       res.status(200).json({message: "add successfull"})
+    }else{
+        res.status(200).json({message: "all ready added"})
+    }
+   } catch (error) {
+    res.status(400).send(error)
+   }
+})
+
+
+app.post('/subject_add', async (req, res)=> {
+   try {
+    var data = await Subject.findOne({name: req.body.name})
+    if (data == null) {
+       var subjectdata = await Subject(req.body);
+       subjectdata.save();
+       await Digree.findOneAndUpdate({_id: req.body.digree}, {$push: {subject:subjectdata._id }})
+       res.status(200).json({message: "add successfull"})
+    }else{
+        res.status(200).json({message: "all ready added"})
+    }
+   } catch (error) {
+    res.status(400).send(error)
+   }
+})
+
+
+
+app.get('/subject', async (req, res)=> {
+   try {
+    var data = await Subject.find({name: {"$regex": req.query.name,"$options": "i"}})
+    res.status(200).send(data)
+   } catch (error) {
+    res.status(400).send(error)
+   }
+})
+
+
+
+
+
 
 
 
