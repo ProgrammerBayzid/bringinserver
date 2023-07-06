bd50baa0e214efdb88293923574ea05c86a51527
ProgrammerBayzid <mdbayzid2622002@gmaile.com>
2023-07-06 11:38:59 +0600

    f

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 const express = require("express");
 const app = express();
-const cors = require("cors");
+const cors = require('cors')
 const bringinRouter = require('./Routers/router')
 const signroute = require('./Routers/Sign_route/sign_route.js')
 const profile = require('./Routers/Profile/profile.js')
@@ -23,7 +23,7 @@ const adminPanel =  require('./Routers/AdminPanel/adminpa')
 const chatrestapi = require('./Routers/Chat/chat_restapi')
 
 app.use(express.json());
-app.use(cors());
+app.use(cors())
 app.use('/uploads', express.static('uploads'))
 app.use(bringinRouter)
 app.use(signroute)
