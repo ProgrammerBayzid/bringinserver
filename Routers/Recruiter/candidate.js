f7bb672c8419002fc652a9cb6b3803544431986b
ProgrammerBayzid <mdbayzid2622002@gmaile.com>
2023-08-15 18:40:49 +0600

    c

--- Routers/Recruiter/candidate.js
+++ Routers/Recruiter/candidate.js
@@ -9,8 +9,8 @@ const JobPost = require("../../Model/Recruiter/Job_Post/job_post.js");
 const candidatesave = require("../../Model/Recruiter/Candidate_Save/candidate_save");
 const candidateReport = require("../../Model/Recruiter/Candidate_Report/candidate_report");
 const { Profiledata } = require("../../Model/Seeker_profile_all_details.js");
-const candidateview = require("../../Model/Recruiter/Candidate_View/candidate_view")
-const { Chat, Message } = require("../../Model/Chat/chat")
+const candidateview = require("../../Model/Recruiter/Candidate_View/candidate_view");
+const { Chat, Message } = require("../../Model/Chat/chat");
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "uploads");
@@ -347,6 +347,7 @@ app.post(
             await candidateReport({
               userid: _id,
               candidateid: req.body.candidateid,
+              candidatefulldetailsid: req.body.candidatefulldetailsid,
               report: req.body.report,
               image: req.file == null ? "" : req.file.path,
               description: req.body.description,
@@ -572,9 +573,7 @@ app.post("/candidate_filter", tokenverify, async (req, res) => {
   }
 });
 
-
-
-app.post("/candidate_view" , tokenverify, async (req, res) => {
+app.post("/candidate_view", tokenverify, async (req, res) => {
   try {
     jwt.verify(req.token, process.env.ACCESS_TOKEN, async (err, authdata) => {
       if (err) {
@@ -595,13 +594,32 @@ app.post("/candidate_view" , tokenverify, async (req, res) => {
             { _id: _id },
             { $inc: { "other.candidate_view": 1 } }
           );
-          var chatchannel = await Chat.findOneAndUpdate({seekerid: req.body.candidate_id, "who_view_me.title": "Who viewed me"}, {$set : {"who_view_me.seekerviewid":_id}, $inc: {"who_view_me.totalview": 1, "who_view_me.newview": 1}})
-                   if(chatchannel == null){
-                    await Chat({type: 3, seekerid: req.body.candidate_id, recruiterid: null, who_view_me: {title: "Who viewed me", totalview: 1,newview: 1, seekerviewid:_id,recruiterview: null}}).save()
-                   } 
+          var chatchannel = await Chat.findOneAndUpdate(
+            {
+              seekerid: req.body.candidate_id,
+              "who_view_me.title": "Who viewed me",
+            },
+            {
+              $set: { "who_view_me.seekerviewid": _id },
+              $inc: { "who_view_me.totalview": 1, "who_view_me.newview": 1 },
+            }
+          );
+          if (chatchannel == null) {
+            await Chat({
+              type: 3,
+              seekerid: req.body.candidate_id,
+              recruiterid: null,
+              who_view_me: {
+                title: "Who viewed me",
+                totalview: 1,
+                newview: 1,
+                seekerviewid: _id,
+                recruiterview: null,
+              },
+            }).save();
+          }
           res.status(200).json({ message: "Candidate view successfully" });
         } else {
-          
           res.status(200).json({ message: "Candidate already view" });
         }
       }
@@ -609,9 +627,6 @@ app.post("/candidate_view" , tokenverify, async (req, res) => {
   } catch (error) {
     res.status(400).send(error);
   }
-})
-
-
-
+});
 
 module.exports = app;
